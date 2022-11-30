60c53d5f5ca4e25f2cd43ba326c9c55d736fb859
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-11-30 15:45:09 -0500

    update template

--- haskell/_dayTemplate/Mod.hs
+++ haskell/_dayTemplate/Mod.hs
@@ -6,12 +6,14 @@ part1 :: IO ()
 part1 = do
   print "part1"
   input <- readInputLines
+  print input
   return ()
 
 part2 :: IO ()
 part2 = do
   print "part2"
   input <- readInputLines
+  print input
   return ()
 
 dispatch :: [(Int, IO ())]
