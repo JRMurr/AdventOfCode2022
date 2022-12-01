1ec866953c877187c3675a8b373550031d2f015e
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-12-01 00:10:27 -0500

    day 1

--- haskell/app/Day01/Mod.hs
+++ haskell/app/Day01/Mod.hs
@@ -1,19 +1,25 @@
 module Day01.Mod where
 
+import Data.List (sortOn)
+import Data.List.Split (splitOn)
 import Utils.Mod
+
+parseInput :: [String] -> [[Int]]
+parseInput s = map (map read) (splitOn [""] s)
 
 part1 :: IO ()
 part1 = do
-  print "part1"
-  input <- readInputLines
-  print input
+  input <- parseInput <$> readInputLines
+  let maxElf = maximum $ map sum input
+  print maxElf
   return ()
 
 part2 :: IO ()
 part2 = do
-  print "part2"
-  input <- readInputLines
-  print input
+  input <- parseInput <$> readInputLines
+  let sorted = sortOn negate $ map sum input
+  let top = take 3 sorted
+  print $ sum top
   return ()
 
 dispatch :: [(Int, IO ())]
