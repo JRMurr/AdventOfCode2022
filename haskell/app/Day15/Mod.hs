98bd0238b85f7f41db75912f8e53b897af2228fb
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-12-16 18:31:35 -0500

    cleanup

--- haskell/app/Day15/Mod.hs
+++ haskell/app/Day15/Mod.hs
@@ -1,12 +1,9 @@
-{-# LANGUAGE LambdaCase #-}
-
 module Day15.Mod where
 
-import Data.List (find, nub)
-import Data.Maybe (isNothing, mapMaybe)
+import Data.List (find)
+import Data.Maybe (isNothing)
 import Data.Set (Set, (\\))
 import qualified Data.Set as Set
-import qualified Debug.Trace as Debug
 import Text.Megaparsec hiding (count)
 import Text.Megaparsec.Char
 import Utils.Coords
@@ -37,54 +34,6 @@ parseLine = label "sensor and becaon" $ do
   beacon <- parseCoord
   pure (sensor, beacon)
 
--- each coord can be a sensort, a becaon, empty (since we know no beacon could be closer), or not checked yet
-data CoordVal = Sensor | Becaon | Empty | NotChecked deriving (Show, Eq)
-
--- type SensorMap = Map Coord CoordVal
-
--- -- given a size calc the points inside the diamond
--- -- TODO: maybe make this recursive and memoize it?
--- -- might still need to cache it
--- getDiamondCoord :: Int -> [Coord]
--- getDiamondCoord size = origin : (leftSide ++ rightSide)
---   where
---     -- we only need to figure out one triangle then roate/flip each point
---     -- which is just size southwest diag lines + the origin
---     getDiagLines n = pointsInLine (addCoord origin (C 0 n)) (addCoord origin (C n 0))
---     lowerRightTriangle = concatMap getDiagLines [1 .. size]
---     upperRightTriag = mapMaybe (\(C y x) -> if y == 0 then Nothing else Just $ C (-y) x) lowerRightTriangle
---     rightSide = lowerRightTriangle ++ upperRightTriag
---     leftSide = mapMaybe (\(C y x) -> if x == 0 then Nothing else Just $ C y (-x)) rightSide
-
--- -- TODO: instead of this, maybe first insert all the pairs
--- -- then count the number of points on the target row have a manhatten <= dist we calc
--- handleSensorBeacon :: SensorMap -> SensorBecaon -> SensorMap
--- handleSensorBeacon sMap (sc, bc) = insertList pairMap [(x, Empty) | x <- emptyPoints]
---   where
---     pairMap = insertList sMap [(sc, Sensor), (bc, Becaon)]
---     dist = manhattan sc bc
---     diamond = getDiamondCoord dist
---     emptyPoints = Debug.trace ("(dist, sc, bc): " ++ show (dist, sc, bc)) $ filter (`Map.notMember` pairMap) $ map (addCoord sc) diamond
-
--- handleAllPairs :: [SensorBecaon] -> SensorMap
--- handleAllPairs = foldl handleSensorBeacon Map.empty
-
--- drawMap :: SensorMap -> String
--- drawMap =
---   drawCoordsGen
---     ( \case
---         Sensor -> 'S'
---         Becaon -> 'B'
---         Empty -> '#'
---         NotChecked -> '.'
---     )
---     NotChecked
-
--- getRow :: SensorMap -> Int -> [CoordVal]
--- getRow sMap row = map (sMap !) coords
---   where
---     coords = filter (\(C y _) -> y == row) (Map.keys sMap)
-
 -- get all points less than the manhattan dist btwn the sensor and beacon at the provided row
 getPointsInRadius :: Int -> SensorBecaon -> Set Int
 getPointsInRadius row (sc@(C sy sx), bc) = Set.fromDistinctAscList [sx + x | x <- range]
@@ -104,13 +53,6 @@ part1 :: IO ()
 part1 = do
   print "part1"
   input <- readInputLinesMapper (parseThrow parseLine)
-  -- print input
-  -- print $ length $ getDiamondCoord 3
-  -- let res = handleAllPairs input
-  -- putStrLn $ drawMap (handleSensorBeacon Map.empty (C (-18) 2, C (-15) (-2)))
-  -- putStrLn $ drawMap res
-  -- print $ count (/= Becaon) $ getRow res 2000000
-  -- print $ length $  getAllNonBeaconPoints 10 input
   print $ Set.size $ getAllNonBeaconPoints 2000000 input
   return ()
 
@@ -152,10 +94,7 @@ part2 :: IO ()
 part2 = do
   print "part2"
   input <- readInputLinesMapper (parseThrow parseLine)
-  -- print input
-  -- print $ getPossibleForPair 20 $ ((C 7 8), (C 10 2))
-  print $ (getTuning 4000000) input
-  -- print $ isInside (C 17 9) ((C 16 9), (C 16 10))
+  print $ getTuning 4000000 input
   return ()
 
 dispatch :: [(Int, IO ())]
