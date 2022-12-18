bf972d75b2321dd900c2e3675d26de08fba605d9
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-12-17 21:57:27 -0500

    day 16 p1

--- haskell/app/Day16/Mod.hs
+++ haskell/app/Day16/Mod.hs
@@ -1,9 +1,8 @@
 module Day16.Mod where
 
-import Data.List (permutations)
+import Data.List (maximumBy)
 import Data.Map (Map, (!))
 import qualified Data.Map as Map
-import Data.Maybe (mapMaybe)
 import qualified Debug.Trace as Debug
 import Text.Megaparsec
 import Text.Megaparsec.Char
@@ -39,11 +38,6 @@ type PathLengths = Map Path Int
 toVMap :: [Valve] -> ValveMap
 toVMap valves = Map.fromList [(vName v, v) | v <- valves]
 
--- could just be a bool but ehh
--- data CurrentValve = Opening Valve | StandingAt Valve deriving (Show)
-
--- data Node = N {openValves :: [Valve], currentValve :: CurrentValve, pressureReleased :: Int}
-
 -- https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm
 
 getPathLength :: PathLengths -> Path -> Int
@@ -64,36 +58,46 @@ calcPathLengths vMap = foldl computeTriplet initMap vertexTriplets
         distKJ = dist k j
         possibleDist = distIK + distKJ
 
-type Node = ([VName], VName, Int)
+type Visited = [VName]
 
--- TODO: gotta have an off by one on step math or something
-getMaxScore :: ValveMap -> PathLengths -> Int -> Node -> Int
-getMaxScore vMap pMap step (visited, curr, currFlowRate)
-  -- these cases might be weird
-  | step > 30 = 0
-  | step == 30 = currFlowRate
-  | otherwise = if null scores then (30 - step) * currFlowRate else maximum scores
+getMaxScore :: ValveMap -> PathLengths -> Int -> Visited -> (Int, [VName])
+getMaxScore vMap pMap step visited
+  | step < 0 = error $ "too many steps: " ++ show step
+  | step == 0 = (0, visited)
+  | otherwise = maxChild
   where
+    curr = head visited
     remaining = filter (`notElem` visited) $ Map.keys vMap
-    checkNeighbor v = flowAddTill + getMaxScore vMap pMap stepWithOpen (v : visited, v, currFlowRate + neighborRate)
+    checkNeighbor v =
+      if dist >= step
+        then (0, visited)
+        else ((numStepsOpen * neighborRate) + subScore, subVis)
       where
         neighborRate = vRate $ vMap ! v
-        numMovesTo = pMap ! (curr, v)
-        flowAddTill = (1 + numMovesTo) * currFlowRate -- might not need the 1+
-        stepWithOpen = 1 + step + numMovesTo -- TODO: what if this goes over 30?
+        dist = pMap ! (curr, v)
+        numStepsOpen = step - dist - 1
+        (subScore, subVis) = getMaxScore vMap pMap numStepsOpen (v : visited)
     scores = map checkNeighbor remaining
+    maxChild =
+      if (not . null) remaining
+        then maximumBy (\(s1, _) (s2, _) -> compare s1 s2) scores
+        else (0, visited)
 
-runGetMax :: ValveMap -> Int
-runGetMax vMap = getMaxScore filtedVMap pMap 0 ([start], start, 0)
+runGetMax :: ValveMap -> (Int, [VName])
+runGetMax vMap = (score, reverse vis)
   where
     pMap = calcPathLengths vMap
     filtedVMap = Map.filter (\v -> vRate v /= 0) vMap
     start = Vname "AA"
+    (score, vis) = getMaxScore filtedVMap pMap 30 [start]
 
 part1 :: IO ()
 part1 = do
   print "part1"
   input <- toVMap <$> readInputLinesParser parseValve
+  -- let pMap = calcPathLengths input
+  -- printNewLine $ Map.toList input
+
   print $ runGetMax input
   -- print $ Map.elems input
   -- printNewLine $ Map.elems input
