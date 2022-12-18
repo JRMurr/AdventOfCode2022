62a5c19d8cf04ddb46d4af4480c462b69a63bb92
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-12-17 22:48:50 -0500

    day 16 p2

--- haskell/app/Day16/Mod.hs
+++ haskell/app/Day16/Mod.hs
@@ -1,8 +1,11 @@
 module Day16.Mod where
 
-import Data.List (maximumBy)
+import Data.Function.Memoize (memoize3)
+import Data.List (delete, maximumBy)
 import Data.Map (Map, (!))
 import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
 import qualified Debug.Trace as Debug
 import Text.Megaparsec
 import Text.Megaparsec.Char
@@ -12,7 +15,7 @@ parseInt :: Parser Int
 parseInt = label "integer" $ read <$> (some numberChar <|> ((:) <$> char '-' <*> some numberChar))
 
 parseVName :: Parser VName
-parseVName = label "vName" $ Vname <$> many letterChar
+parseVName = label "vName" $ many letterChar
 
 parseValve :: Parser Valve
 parseValve = label "valve" $ do
@@ -25,7 +28,8 @@ parseValve = label "valve" $ do
   tunnels <- sepBy parseVName (string ", ")
   pure (V {vName = name, vRate = rate, vTunnels = tunnels})
 
-newtype VName = Vname String deriving (Show, Ord, Eq)
+-- newtype VName = Vname String deriving (Show, Ord, Eq)
+type VName = String
 
 data Valve = V {vName :: VName, vRate :: Int, vTunnels :: [VName]} deriving (Show)
 
@@ -60,55 +64,85 @@ calcPathLengths vMap = foldl computeTriplet initMap vertexTriplets
 
 type Visited = [VName]
 
-getMaxScore :: ValveMap -> PathLengths -> Int -> Visited -> (Int, [VName])
-getMaxScore vMap pMap step visited
+type Remaining = Set VName
+
+getMaxScore :: ValveMap -> PathLengths -> Int -> VName -> [VName] -> Int
+getMaxScore vMap pMap step curr remaining
   | step < 0 = error $ "too many steps: " ++ show step
-  | step == 0 = (0, visited)
+  | step == 0 = 0
   | otherwise = maxChild
   where
-    curr = head visited
-    remaining = filter (`notElem` visited) $ Map.keys vMap
     checkNeighbor v =
       if dist >= step
-        then (0, visited)
-        else ((numStepsOpen * neighborRate) + subScore, subVis)
+        then 0
+        else (numStepsOpen * neighborRate) + subScore
       where
         neighborRate = vRate $ vMap ! v
         dist = pMap ! (curr, v)
         numStepsOpen = step - dist - 1
-        (subScore, subVis) = getMaxScore vMap pMap numStepsOpen (v : visited)
+        subScore = getMaxScore vMap pMap numStepsOpen v (delete v remaining)
     scores = map checkNeighbor remaining
     maxChild =
       if (not . null) remaining
-        then maximumBy (\(s1, _) (s2, _) -> compare s1 s2) scores
-        else (0, visited)
+        then maximum scores
+        else 0
 
-runGetMax :: ValveMap -> (Int, [VName])
-runGetMax vMap = (score, reverse vis)
+type MemHelper = Int -> VName -> [VName] -> Int
+
+getMaxScoreMem :: ValveMap -> PathLengths -> MemHelper
+getMaxScoreMem vMap pMap = memoize3 (getMaxScore vMap pMap)
+
+start :: VName
+start = "AA"
+
+runGetMax :: ValveMap -> Int
+runGetMax vMap = getMaxScore filtedVMap pMap 30 start (Map.keys filtedVMap)
   where
     pMap = calcPathLengths vMap
     filtedVMap = Map.filter (\v -> vRate v /= 0) vMap
-    start = Vname "AA"
-    (score, vis) = getMaxScore filtedVMap pMap 30 [start]
+
+getMaxP2 :: ValveMap -> PathLengths -> MemHelper -> Int -> VName -> Remaining -> Int
+getMaxP2 vMap pMap memHelper step curr remaining
+  | step < 0 = error $ "too many steps: " ++ show step
+  | step == 0 = 0
+  | otherwise = maxChild
+  where
+    checkNeighbor v =
+      if dist >= step
+        then 0
+        else (numStepsOpen * neighborRate) + subScore
+      where
+        neighborRate = vRate $ vMap ! v
+        dist = pMap ! (curr, v)
+        numStepsOpen = step - dist - 1
+        subScore = getMaxP2 vMap pMap memHelper numStepsOpen v (Set.delete v remaining)
+    scoresSelf = Set.map checkNeighbor remaining
+    scoreElf = memHelper 26 start (Set.toAscList remaining)
+    joinedScores = Set.insert scoreElf scoresSelf
+    maxChild =
+      if (not . null) remaining
+        then Set.findMax joinedScores
+        else 0
+
+runGetMaxP2 :: ValveMap -> Int
+runGetMaxP2 vMap = getMaxP2 filtedVMap pMap getMaxMem 26 start (Set.fromList (Map.keys filtedVMap))
+  where
+    pMap = calcPathLengths vMap
+    filtedVMap = Map.filter (\v -> vRate v /= 0) vMap
+    getMaxMem = getMaxScoreMem filtedVMap pMap
 
 part1 :: IO ()
 part1 = do
   print "part1"
   input <- toVMap <$> readInputLinesParser parseValve
-  -- let pMap = calcPathLengths input
-  -- printNewLine $ Map.toList input
-
   print $ runGetMax input
-  -- print $ Map.elems input
-  -- printNewLine $ Map.elems input
-  -- print $ length $ getAllOrderings input
   return ()
 
 part2 :: IO ()
 part2 = do
   print "part2"
   input <- toVMap <$> readInputLinesParser parseValve
-  print input
+  print $ runGetMaxP2 input
   return ()
 
 dispatch :: [(Int, IO ())]
